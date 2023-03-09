6f855183b3df907ada59ed2cfb1beae754d85dcb
TakeAsaHaru <geronimo_family@icloud.com>
2023-03-09 15:00:30 +0900

    ホーム画面にログイン/タスクコンポーネント追加

--- pochiren-confirm-app/pages/home.tsx
+++ pochiren-confirm-app/pages/home.tsx
@@ -1,10 +1,16 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Divider, Heading } from "@chakra-ui/react";
 import { Login } from "./components/login";
+import { Task } from "./components/task";
 
 export default function Home() {
   return (
-    <Box w={1000} m={10}>
+    <Box w={1500} m={10}>
+      <Heading>ログイン機能確認</Heading>
       <Login />
+      <Divider borderColor="blue" mb={10} />
+      <Heading>タスク関連機能確認</Heading>
+      <Task />
+      <Divider borderColor="blue" mb={10} />
     </Box>
   );
 }
