885fc65c77d88cb1f3636036b92ef685b24d0fd8
TakeAsaHaru <geronimo_family@icloud.com>
2023-03-14 11:52:09 +0900

    ホーム画面にUserコンポーネント追加

--- pochiren-confirm-app/pages/home.tsx
+++ pochiren-confirm-app/pages/home.tsx
@@ -1,16 +1,19 @@
 import { Box, Divider, Heading } from "@chakra-ui/react";
 import { Login } from "./components/login";
 import { Task } from "./components/task";
+import { User } from "./components/user";
 
 export default function Home() {
   return (
     <Box w={1500} m={10}>
       <Heading>ログイン機能確認</Heading>
       <Login />
-      <Divider borderColor="blue" mb={10} />
+      {/* <Divider borderColor="blue" mb={10} />
       <Heading>タスク関連機能確認</Heading>
       <Task />
       <Divider borderColor="blue" mb={10} />
+      <Heading>ユーザー登録機能確認</Heading>
+      <User /> */}
     </Box>
   );
 }
