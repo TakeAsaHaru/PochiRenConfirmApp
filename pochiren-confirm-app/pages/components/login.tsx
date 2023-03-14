f5136db68542fbb88b94751eb5f46cac7f7ae5bd
TakeAsaHaru <geronimo_family@icloud.com>
2023-03-14 11:53:13 +0900

    ディレクトリ構成変更

--- pochiren-confirm-app/pages/components/login.tsx
+++ pochiren-confirm-app/pages/components/login.tsx
@@ -14,9 +14,13 @@ import {
   Flex,
   Spacer,
   Heading,
+  Divider,
+  Tab,
 } from "@chakra-ui/react";
 
 import jsondjangoapp from "../api/jsondjangoapp";
+import { Task } from "./task";
+import { User } from "./user";
 
 type LoginInfo = {
   email: string;
@@ -83,70 +87,113 @@ export function Login() {
   };
 
   return (
-    <Flex direction="row" align="start" mb={10}>
-      <Box w="30%" mb={10}>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <FormControl>
-            <FormLabel>Email</FormLabel>
-            <Input
-              id="email"
-              type="email"
-              {...register("email", { required: "メールアドレスは必須です" })}
-            />
-            <FormErrorMessage>
-              {errors.email && errors.email.message}
-            </FormErrorMessage>
-          </FormControl>
-          <FormControl>
-            <FormLabel>password</FormLabel>
-            <Input
-              id="password"
-              type="password"
-              {...register("password", { required: "パスワードは必須です" })}
-            />
-            <FormErrorMessage>
-              {errors.password && errors.password.message}
-            </FormErrorMessage>
-          </FormControl>
-          <Box>
-            <Button type="submit" colorScheme="teal" mt={5} w="100%">
-              Submit
-            </Button>
-            <Button
-              type="button"
-              colorScheme="blue"
-              mt={5}
-              w="100%"
-              onClick={getUser}
-            >
-              GET
-            </Button>
-          </Box>
-        </form>
-      </Box>
-      <Box maxW="md" ml={10}>
-        <Flex direction="column">
-          <Heading size="md" mb={3}>
-            ユーザー情報
-          </Heading>
-          <Text>access:</Text>
-          <Text mb={5}>{token?.access}</Text>
-          <Text>refresh:</Text>
-          <Text mb={5}>{token?.refresh}</Text>
-          <Text>user:</Text>
-          <Text>ID:{user?.id}</Text>
-          <Text>staff_num:{user?.staff_num}</Text>
-          <Text>email:{user?.email}</Text>
-          <Text>name:{user?.name}</Text>
-          <Text>password:{user?.password}</Text>
-          <Text>is_active:{user?.is_active ? "True" : "False"}</Text>
-          <Text>is_staff:{user?.is_staff ? "True" : "False"}</Text>
-          <Text>is_manager:{user?.is_manager ? "True" : "False"}</Text>
-          <Text>login_at:{String(user?.login_at)}</Text>
-          <Text>not_active_at:{String(user?.not_active_at)}</Text>
-          <Text>created_at:{String(user?.created_at)}</Text>
-        </Flex>
-      </Box>
-    </Flex>
+    <Box>
+      <Flex direction="row" align="start" mb={10}>
+        <Box w="30%" mb={10}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <FormControl>
+              <FormLabel>Email</FormLabel>
+              <Input
+                id="email"
+                type="email"
+                {...register("email", { required: "メールアドレスは必須です" })}
+              />
+              <FormErrorMessage>
+                {errors.email && errors.email.message}
+              </FormErrorMessage>
+            </FormControl>
+            <FormControl>
+              <FormLabel>password</FormLabel>
+              <Input
+                id="password"
+                type="password"
+                {...register("password", { required: "パスワードは必須です" })}
+              />
+              <FormErrorMessage>
+                {errors.password && errors.password.message}
+              </FormErrorMessage>
+            </FormControl>
+            <Box>
+              <Button type="submit" colorScheme="teal" mt={5} w="100%">
+                Submit
+              </Button>
+              <Button
+                type="button"
+                colorScheme="blue"
+                mt={5}
+                w="100%"
+                onClick={getUser}
+              >
+                GET
+              </Button>
+            </Box>
+          </form>
+        </Box>
+        <Box maxW="md" ml={10}>
+          <Flex direction="column">
+            <Heading size="md" mb={3}>
+              トークン情報
+            </Heading>
+            <Text fontWeight="bold">access:</Text>
+            <Text mb={5}>{token?.access}</Text>
+            <Text fontWeight="bold">refresh:</Text>
+            <Text mb={5}>{token?.refresh}</Text>
+            <Heading size="md" mb={3}>
+              ユーザー情報
+            </Heading>
+            <HStack>
+              <Text fontWeight="bold">ID:</Text>
+              <Text>{user?.id}</Text>
+            </HStack>
+            <HStack>
+              <Text fontWeight="bold">staff_num:</Text>
+              <Text>{user?.staff_num}</Text>
+            </HStack>
+            <HStack>
+              <Text fontWeight="bold">email:</Text>
+              <Text>{user?.email}</Text>
+            </HStack>
+            <HStack>
+              <Text fontWeight="bold">name:</Text>
+              <Text>{user?.name}</Text>
+            </HStack>
+            <HStack>
+              <Text fontWeight="bold">password:</Text>
+              <Text>{user?.password}</Text>
+            </HStack>
+            <HStack>
+              <Text fontWeight="bold">is_active:</Text>
+              <Text>{user?.is_active ? "True" : "False"}</Text>
+            </HStack>
+            <HStack>
+              <Text fontWeight="bold">is_staff:</Text>
+              <Text>{user?.is_staff ? "True" : "False"}</Text>
+            </HStack>
+            <HStack>
+              <Text fontWeight="bold">is_manager:</Text>
+              <Text>{user?.is_manager ? "True" : "False"}</Text>
+            </HStack>
+            <HStack>
+              <Text fontWeight="bold">login_at:</Text>
+              <Text>{String(user?.login_at)}</Text>
+            </HStack>
+            <HStack>
+              <Text fontWeight="bold">not_active_at:</Text>
+              <Text>{String(user?.not_active_at)}</Text>
+            </HStack>
+            <HStack>
+              <Text fontWeight="bold">created_at:</Text>
+              <Text>{String(user?.created_at)}</Text>
+            </HStack>
+          </Flex>
+        </Box>
+      </Flex>
+      <Divider borderColor="blue" mb={10} />
+      <Heading>タスク関連機能確認</Heading>
+      <Task userId={user?.id} />
+      <Divider borderColor="blue" mb={10} />
+      <Heading>ユーザー登録機能確認</Heading>
+      <User />
+    </Box>
   );
 }
